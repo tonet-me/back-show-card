6365b0db6b4b6afd9674690c9e1d55fa360ba46e
masood <keshvari.developer@gmail.com>
2021-11-08 14:34:42 +0330

    fix cors

--- routes/routes.go
+++ routes/routes.go
@@ -5,13 +5,13 @@ import (
 	env "Card-Request-Manager/env"
 	"net/http"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func StartGin() {
 	router := gin.Default()
-	router.Use(cors.Default())
+	// router.Use(cors.Default())
+	router.Use(CORSMiddleware())
 	router.Use(gin.Recovery())
 	router.GET("qr/:qrCode", card.GetCardByQrcode)
 	router.GET("/:cardName", card.GetCardByUsername)
@@ -20,4 +20,20 @@ func StartGin() {
 	})
 	port := env.GoDotEnvVariable("PORT")
 	router.Run(port)
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
 }
